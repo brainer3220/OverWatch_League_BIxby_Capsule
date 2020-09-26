9e54f81bffb95d4d4c2bd761b374243dcd28f2f9
brainer <jeansanghyun@gmail.com>
2020-09-27 02:06:12 +0900

    Short nicely

--- code/Find_All_Team.js
+++ code/Find_All_Team.js
@@ -3,7 +3,7 @@ module.exports.function = function find_All_Team () {
   var console = require('console');
   const http = require ("http");
 
-  TeamID = [ 7698, 4402, 7692, 4407, 4523, 7699, 7693, 4525, 4410, 4406, 4405, 4403, 7694, 4524, 4404, 4409, 4408, 7695, 7696, 7697]
+  TeamID = [ 7698, 4402, 7692, 4407, 4523, 7699, 7693, 4525, 4410, 4406, 4405, 4403, 7694, 4524, 4404, 4409, 4408, 7695, 7696, 7697 ]
 
   let options = {
     format: 'json',
