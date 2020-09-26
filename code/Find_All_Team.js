e3916c77a75ccc863bdc51e7316e0d707f43f296
brainer <jeansanghyun@gmail.com>
2020-09-27 02:05:51 +0900

    Logic 변경

--- code/Find_All_Team.js
+++ code/Find_All_Team.js
@@ -1,10 +1,9 @@
-module.exports.function = function find_All_Team (Team_Name) {
+module.exports.function = function find_All_Team () {
 
   var console = require('console');
   const http = require ("http");
 
-  let Team_Name
-  let ret = []
+  TeamID = [ 7698, 4402, 7692, 4407, 4523, 7699, 7693, 4525, 4410, 4406, 4405, 4403, 7694, 4524, 4404, 4409, 4408, 7695, 7696, 7697]
 
   let options = {
     format: 'json',
@@ -12,9 +11,21 @@ module.exports.function = function find_All_Team (Team_Name) {
       'accept': 'application/json'
     },
   };
+  
+  result = []
+  for (let i = 0; i < TeamID.length; ++i) {
+    // console.log('https://api.overwatchleague.com/v2/teams/' + TeamID[i])
+    var response = http.getUrl('https://api.overwatchleague.com/v2/teams/' + TeamID[i] , options);
+    data = response.data
+    var TeamInfo = {
+      team_name: data.name,
+      player: data.players,
+      social_media: data.accounts
+    }
+    result.push(TeamInfo);
+  }
 
-  let response = http.getUrl('https://api.overwatchleague.com/v2/teams/4409', options);
-  console.log("response: ", response);
+  console.log(result)
 
-  return response
+  return result
 }
